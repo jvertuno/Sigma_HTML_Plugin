63336750110c479268e348b640ae27c8c725d90c
jvertuno <jonvertuno@gmail.com>
2025-10-31 16:52:36 -0400

    Add files via upload

--- Sigma Plug In Code/src/App.jsx
+++ Sigma Plug In Code/src/App.jsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useElementData, useElementColumns } from '@sigmacomputing/plugin';
 
 function App() {
   const data = useElementData();
   const columns = useElementColumns();
 
-  useEffect(() => {
-    console.log('Sigma Plugin Debug Log');
-    console.log('Data:', data);
-    console.log('Columns:', columns);
-  }, [data, columns]);
-
   if (!data || data.length === 0) return <div>No data available</div>;
 
-  const rawText = data[0][columns[0]?.name];
+  const rawText = data[0][columns[0].name];
   const htmlContent = `<div style="font-family:Arial; font-size:16px; color:#333;">${rawText}</div>`;
 
   return (
@@ -21,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default A
