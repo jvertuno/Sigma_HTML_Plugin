2e25825b0ff216c0aa1d861617273ef3ebffb5fe
jvertuno <jonvertuno@gmail.com>
2025-10-31 16:38:46 -0400

    Update App.jsx

--- Sigma Plug In Code/src/App.jsx
+++ Sigma Plug In Code/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useElementData, useElementColumns } from '@sigmacomputing/plugin';
 
 function App() {
   const data = useElementData();
   const columns = useElementColumns();
 
+  useEffect(() => {
+    console.log('Sigma Plugin Debug Log');
+    console.log('Data:', data);
+    console.log('Columns:', columns);
+  }, [data, columns]);
+
   if (!data || data.length === 0) return <div>No data available</div>;
 
-  const rawText = data[0][columns[0].name];
+  const rawText = data[0][columns[0]?.name];
   const htmlContent = `<div style="font-family:Arial; font-size:16px; color:#333;">${rawText}</div>`;
 
   return (
@@ -15,4 +21,4 @@ function App() {
   );
 }
 
-export default A
+export default App;
